70557b9ca02c822b4024600cde860345a641a687
RussianStupidCode <67269808+RussianStupidCode@users.noreply.github.com>
2022-09-19 18:49:21 +0000

    Deploying to gh-pages from @ RussianStupidCode/ra-context@f73aab2d0aa63dbfa371800c966d20b16e3fb904 🚀

---
+++ precache-manifest.8088eee592cb6415626f41e95dcf3f23.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "4648585a2b2f9f30fb65",
+    "url": "/ra-context/static/css/main.91c530ee.chunk.css"
+  },
+  {
+    "revision": "4648585a2b2f9f30fb65",
+    "url": "/ra-context/static/js/main.39bbbaea.chunk.js"
+  },
+  {
+    "revision": "4429482da2377c1e1dc7",
+    "url": "/ra-context/static/js/runtime~main.91466459.js"
+  },
+  {
+    "revision": "e0eaacefb03c8d55c1f4",
+    "url": "/ra-context/static/css/2.727ab9e3.chunk.css"
+  },
+  {
+    "revision": "e0eaacefb03c8d55c1f4",
+    "url": "/ra-context/static/js/2.6b86c1d6.chunk.js"
+  },
+  {
+    "revision": "31e1300d419245fd27614630601dc74d",
+    "url": "/ra-context/static/media/bootstrap-icons.31e1300d.woff2"
+  },
+  {
+    "revision": "be6df3f0c9855796ae0c099ee4ca43ce",
+    "url": "/ra-context/static/media/bootstrap-icons.be6df3f0.woff"
+  },
+  {
+    "revision": "e21dc35004fc04e0237340539c1fa9d5",
+    "url": "/ra-context/index.html"
+  }
+];
